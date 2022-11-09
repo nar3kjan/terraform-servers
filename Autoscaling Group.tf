7631e13b9e3026edb03db243b07aa9edc42ab388
Narek <zzz@zzz.zzz>
2022-11-09 12:01:17 +0400

    HTTP_HTTPS

--- Autoscaling Group.tf
+++ Autoscaling Group.tf
@@ -136,14 +136,6 @@ resource "aws_lb" "web" {
 
   enable_deletion_protection = true
 
- 
-  health_check {
-    healthy_threshold = 2
-    unhealthy_threshold = 2
-    timeout = 3
-    target = "HTTP:80/"
-    interval = 10
-  }
 
   tags = {
     Name = "WebServer-Highly_Available_ALB"
