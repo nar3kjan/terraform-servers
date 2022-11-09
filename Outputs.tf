d1c05645b3410cfacf6324af37f6869ad232f98e
Narek <zzz@zzz.zzz>
2022-11-09 12:00:15 +0400

    HTTP_HTTPS

--- Outputs.tf
+++ Outputs.tf
@@ -1,11 +1,11 @@
 output "elb-id" {
-  value = aws_elb.web.id
+  value = aws_lb.web.id
 }
 
 output "elb_zone_id" {
-  value = aws_elb.web.zone_id
+  value = aws_lb.web.zone_id
 }
 
 output "elb_dns_name" {
-  value = aws_elb.web.dns_name
+  value = aws_lb.web.dns_name
 }
